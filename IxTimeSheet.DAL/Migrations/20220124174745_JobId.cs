69b0b5d5d6e88e74d9037f2baef12ea2d8884aed
Siddesh Bagare <siddeshbagare@gmail.com>
2022-01-24 23:21:08 +0530

    PId FK

---
+++ IxTimeSheet.DAL/Migrations/20220124174745_JobId.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IxTimeSheet.DAL.Migrations
+{
+    public partial class JobId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PId",
+                table: "Jobs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_PId",
+                table: "Jobs",
+                column: "PId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_Clients_PId",
+                table: "Jobs",
+                column: "PId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_Clients_PId",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_PId",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "PId",
+                table: "Jobs");
+        }
+    }
+}
--- IxTimeSheet.DAL/Model/Job.cs
+++ IxTimeSheet.DAL/Model/Job.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IxTimeSheet.DAL.Model
 {
@@ -9,9 +10,14 @@ namespace IxTimeSheet.DAL.Model
         [Key]
         public int Id { get; set; }
 
+        public int PId { get; set; }
+
         [DisplayName("Job Name")]
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
+
+        [ForeignKey("PId")]
+        public virtual Client Client { get; set; }
     }
 }
